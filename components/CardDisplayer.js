e2b2a75b33ff081fb30a88f959244b57ea74858f
DystDev <58528580+DystDev@users.noreply.github.com>
2022-04-15 15:04:54 +0400

    No More FlatList, CardDisplayer
    - Replace with scrollview
    - New component (CardDisplayer) that returns card

---
+++ components/CardDisplayer.js
@@ -0,0 +1,10 @@
+import { View, Text, Dimensions } from 'react-native';
+import { styles } from '../styles/styles';
+import Card from './Card';
+
+export default CardDisplayer = (props) => {
+  const colors = props.colors;
+  return colors.map((data) => {
+    return <Card color={data.color} colorName={data.colorName} key={data.id} />;
+  });
+};
--- screens/GeneratorScreen.js
+++ screens/GeneratorScreen.js
@@ -1,29 +1,26 @@
 import React, { useState } from 'react';
-import { View, FlatList, Text } from 'react-native';
+import { View, FlatList, Text, ScrollView } from 'react-native';
 import { styles } from '../styles/styles';
-import Card from '../components/Card';
+import CardDisplayer from '../components/CardDisplayer';
 
 export default GeneratorScreen = () => {
   const [colors, setColors] = useState([
     { color: '#f0f', id: '1', colorName: 'yellow' },
-    { color: '#f0f0f0', id: '2', colorName: 'idk white or something?' }
+    { color: '#435636', id: '2', colorName: 'idk white or something?' },
+    { color: '#875913', id: '3', colorName: 'broen lol' },
+    { color: '#132615', id: '4', colorName: 'idk white or something?' },
+    { color: '#f00022', id: '5', colorName: 'idk white or something?' },
+    { color: '#991223', id: '6', colorName: 'idk white or something?' },
+    { color: '#8600ff', id: '7', colorName: 'idk white or something?' }
   ]);
 
   const generateColorsHandler = () => {};
 
-  const renderCard = ({ item }) => {
-    <Card color={item.color} name={item.colorName} />;
-  };
-
   return (
     <View style={[styles.screen, styles.generatorScreen]}>
-      {/* <FlatList
-        data={colors}
-        renderItem={renderCard}
-        keyExtractor={(item) => item.id}
-      /> */}
-      <Card color={colors[0].color} colorName={colors[0].colorName} />
-      <Card color={colors[0].color} colorName={colors[0].colorName} />
+      <ScrollView>
+        <CardDisplayer colors={colors} />
+      </ScrollView>
     </View>
   );
 };
