eb4f16d3b50f1359cd9e39c46a92e782b969a59e
DystDev <58528580+DystDev@users.noreply.github.com>
2022-04-25 22:01:35 +0100

    Add alternate logo

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,10 +1,13 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import { styles } from '../styles/styles';
 
 export default GeneratorScreen = () => {
   return (
     <View style={[styles.screen, styles.generatorScreen]}>
-      <Text>By DystDev 2022</Text>
+      <Image
+        source={require('../assets/logo-alt.png')}
+        style={{ width: 50, height: 50 }}
+      />
     </View>
   );
 };
