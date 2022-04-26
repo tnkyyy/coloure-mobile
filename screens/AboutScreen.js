94aeafea285e0bb4bc123f514991cab87abcf65a
DystDev <58528580+DystDev@users.noreply.github.com>
2022-04-26 19:54:06 +0100

    About Screen
    - Update logo
    - Add text

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,13 +1,21 @@
 import { View, Text, Image } from 'react-native';
 import { styles } from '../styles/styles';
+import * as Linking from 'expo-linking';
 
 export default GeneratorScreen = () => {
   return (
     <View style={[styles.screen, styles.generatorScreen]}>
       <Image
         source={require('../assets/logo-alt.png')}
-        style={{ width: 50, height: 50 }}
+        style={{ width: 300, height: 100 }}
       />
+      <Text style={[styles.cardTextSub, { margin: 10, textAlign: 'center' }]}>
+        Hi there! I'm Rowan, the creator of Coloure. I hope you have been able
+        to enjoy using the app for whatever you need it for!
+      </Text>
+      <Text style={[styles.cardTextSub, { margin: 10, textAlign: 'center' }]}>
+        This app is still relatively new and will be maintained, adding more generation algorithms and features in the future.
+      </Text>
     </View>
   );
 };
