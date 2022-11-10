3ee8b88bb430715a6fa0e0a284597f6f4fb2ceca
DystDev <58528580+DystDev@users.noreply.github.com>
2022-11-10 08:26:55 +0000

    Sharing Screen

--- screens/ShareScreen.js
+++ screens/ShareScreen.js
@@ -2,8 +2,22 @@ import { View, Text, Image } from 'react-native';
 import { styles } from '../styles/styles';
 import * as Linking from 'expo-linking';
 import Anchor from '../components/Anchor';
+import { useRef } from 'react';
 
-export default AboutScreen = () => {
+export default ShareScreen = () => {
+  ref = useRef();
+
+  const shareData = () => {
+    ref.current.capture().then((uri) => {
+      Sharing.shareAsync(uri).catch((err) => {
+        Toast.show({
+          type: 'error',
+          text1: 'Sharing failed. Do you have any colors?'
+        });
+      });
+    });
+  };
+
   return (
     <View
       style={
